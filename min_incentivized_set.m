bcdf4e2cbf117b4f61b4a2204a20174cc9a6760a
Misha Khodak <mkhodak@princeton.edu>
2016-01-12 03:32:41 -0500

    updated SDP

--- min_incentivized_set.m
+++ min_incentivized_set.m
@@ -2,7 +2,7 @@ p = [4234; 1603; 9; 5; 1; 28; 1; 1; 59; 17; 9; 1; ...
      81; 288; 1607; 2; 1; 13; 139; 90; 78; 164; 35];
 n = length(p);
 Z = (1.1-1)/(1-.05);
-C = log(p)+1;
+C = (log(p)+1)/mean(log(p)+1);
 W = zeros(n);
 for i = 1:n
     for j = 1:n
@@ -17,16 +17,15 @@ for i = 1:n
     P(:,:,i) = Z*E(:,i)*M(:,i)';
     q(:,i) = -Z*M(:,i)-C(i)*E(:,i);
 end
-
 cvx_begin sdp
     variable X(n,n) symmetric
     variable x(n)
-    minimize(trace(M'*M*X))
+    minimize(trace(W'*W*X)-C'*x)
     subject to
     x <= 1;
     x >= 0;
     [[X, x]; [x', 1]] >= semidefinite(n+1);
     for i = 1:n
-        trace((P(:,:,i)+P(:,:,i)')'*X)+q(:,i)'*x+C(i) <= 0;
+        trace((P(:,:,i)+P(:,:,i)')'*X)+2*q(:,i)'*x+2*C(i) <= 0;
     end
 cvx_end
